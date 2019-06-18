ec44d9e20a9d68440c3c8dd3fa8d09970ef5cb74
lyz21 <liuyenzhe@163.com>
2019-06-18 14:48:26 +0800

    更新元组测试

---
+++ Tuple.py
@@ -0,0 +1,49 @@
+"""
+元组测试
+    -元组与列表基本类似，但是元组不可更改，列表可更改
+"""
+print('创建元组----------------------------')
+tup1 = ('stu1', 'stu2', 'stu3', 'stu4')  # String类型元组
+tup2 = (1, 2, 3, 4, 5, 6, 7, 8)  # 整型类型元组
+tup3 = 'user1', 'user2', 'user3', 'user4'  # 不需要括号也可以
+tup4 = (1,)  # 如果元组内只有一个元素，需要在后面加上','，否则会当成整型处理
+print('输出元组----------------------------')
+print('元组1：', tup1)
+print('元组2：', tup2)
+print('元组3：', tup3)
+print('元组4：', tup4)
+print('元组2的第2到4个元素：', tup2[1:4])
+print('连接元组----------------------------')
+tup5 = tup1 + tup2
+print(tup5)
+print('删除元组----------------------------')
+# 列表可删除其中某个元素，元组只能删除整个元组
+# del tup4  #先注释掉，会报错
+print(tup4)
+print('元组运算符----------------------------')
+'''
+len()   元素个数
++       连接
+*       复制
+in      元素是否存在
+for x in tup : print(x) 迭代
+'''
+a = 10
+if a in tup2:
+    print('{}在元组tup2中'.format(a))
+else:
+    print('{}不在元组tup2中'.format(a))
+print('tup5:')
+for x in tup5:
+    print(x)
+print('元组内置函数----------------------------')
+'''
+len()   元素个数
+max()   元组中元素最大值
+min()   最小元素
+tuple() 列表转元组
+'''
+print('tup2:', tup2)
+print('len(tup2):', len(tup2))
+print('max(tup2):', max(tup2))
+print('min(tup2):', min(tup2))
--- test1.py
+++ test1.py
@@ -1,85 +1,81 @@
 print("字符串测试-----------------------------------")
-str="123"           # "" 和 '' 效果一样
-str2="abc"
-var1=",.?"
-print(str)          #输出字符串
-print(str2)         #输出字符串
-print(var1)         #输出字符串
-print(str[0:-1])    #输出第一个到 倒数第二个
-print(str[-1])      #输出倒数第一个
-print(str*2)        #输出字符串两次
-print(str+'hello')  #连接字符串
+str = "123"  # "" 和 '' 效果一样
+str2 = "abc"
+var1 = ",.?"
+print(str)  # 输出字符串
+print(str2)  # 输出字符串
+print(var1)  # 输出字符串
+print(str[0:-1])  # 输出第一个到 倒数第二个
+print(str[-1])  # 输出倒数第一个
+print(str * 2)  # 输出字符串两次
+print(str + 'hello')  # 连接字符串
 print("输出测试-----------------------------------")
-a='a'
-b='b'
-print(a)            #换行输出
+a = 'a'
+b = 'b'
+print(a)  # 换行输出
 print(b)
-print(a,end='')     #不换行输出
+print(a, end='')  # 不换行输出
 print(b)
 print("number数据类型测试-----------------------------------")
-ZhengShu = 100          # 整型变量
-FuDian   = 1000.0       # 浮点型变量
-FuShu    = 3.14j        #复数类型   复数：z=a+bi
-BuEr     = True         #布尔类型   flase 0 ；true 1
-ZiFuChuan= "runoob"     # 字符串
+ZhengShu = 100  # 整型变量
+FuDian = 1000.0  # 浮点型变量
+FuShu = 3.14j  # 复数类型   复数：z=a+bi
+BuEr = True  # 布尔类型   flase 0 ；true 1
+ZiFuChuan = "runoob"  # 字符串
 
 print(ZhengShu)
 print(FuDian)
-print(FuShu+1)
+print(FuShu + 1)
 print(BuEr)
 print(ZiFuChuan)
 print("list列表测试----Tuple元组与list列表用法一样，将[]换为()即可。不同处在元组不可改变，list列表可改变")
-Liebiao = ['abcd','1234',',.?'];
-list=['列表',123];
-print(Liebiao)      #输出完整列表
-print(Liebiao[0])   #输出列表第一个元素
-print(Liebiao[-1])  #输出最后一个元素
-print(Liebiao[1:])  #输出从第二个到最后
-print(Liebiao*2)    #输出两份
-print(Liebiao+list) #两个列表联合
-print(Liebiao[0][0])#输出列表第一个元素的第一个字母
-print(Liebiao[0::2])#输出从第一个到最后一个，步长为2输出
+Liebiao = ['abcd', '1234', ',.?'];
+list = ['列表', 123];
+print(Liebiao)  # 输出完整列表
+print(Liebiao[0])  # 输出列表第一个元素
+print(Liebiao[-1])  # 输出最后一个元素
+print(Liebiao[1:])  # 输出从第二个到最后
+print(Liebiao * 2)  # 输出两份
+print(Liebiao + list)  # 两个列表联合
+print(Liebiao[0][0])  # 输出列表第一个元素的第一个字母
+print(Liebiao[0::2])  # 输出从第一个到最后一个，步长为2输出
 print("set集合测试-----------------------------------")
-student={'小明','小刚','小强','小张','小强'}
-print(student)#set不可重复，输出直接去除重复元素
-if '小明'in student:
+student = {'小明', '小刚', '小强', '小张', '小强'}
+print(student)  # set不可重复，输出直接去除重复元素
+if '小明' in student:
     print("小明在student中")
 else:
     print("小明不在student中")
-a=set('1234567890一二三四')
-b=set('1234567890abcdefg')
-print(a-b)  #差集
-print(a|b)  #并集
-print(a&b)  #交集
-print(a^b)  #a,b中不同时存在
+a = set('1234567890一二三四')
+b = set('1234567890abcdefg')
+print(a - b)  # 差集
+print(a | b)  # 并集
+print(a & b)  # 交集
+print(a ^ b)  # a,b中不同时存在
 print("Dictionary字典测试-----------------------------------")
-dict={} #空字典
-dict['lyz']="刘言哲"
-dict['yjj']='于佳佳'
-dilts={'name':'值','age':18}
-print(dict['lyz'])  #输出键为lyz的值
-print(dict['yjj'])  #输出键为yjj的值
-print(dilts.keys()) #输出字典中所有的键
-print(dilts.values())   #输出字典中所4有的值
-print (dilts)#输出完整的字典
+dict = {}  # 空字典
+dict['lyz'] = "刘言哲"
+dict['yjj'] = '于佳佳'
+dilts = {'name': '值', 'age': 18}
+print(dict['lyz'])  # 输出键为lyz的值
+print(dict['yjj'])  # 输出键为yjj的值
+print(dilts.keys())  # 输出字典中所有的键
+print(dilts.values())  # 输出字典中所4有的值
+print(dilts)  # 输出完整的字典
 print("运算符测试-----------------------------------")
-a=2
-b=5
-print('a+b=',a+b)  #加法
-print('a-b=',a-b)  #减法
-print('a*b=',a*b)  #乘法
-print('a/b=',a/b)  #除法
-print('a%b=',a%b)  #取余
-print('a**b=',a**b) #幂
-print('a//b=',a//b) #取商的整数部分
+a = 2
+b = 5
+print('a+b=', a + b)  # 加法
+print('a-b=', a - b)  # 减法
+print('a*b=', a * b)  # 乘法
+print('a/b=', a / b)  # 除法
+print('a%b=', a % b)  # 取余
+print('a**b=', a ** b)  # 幂
+print('a//b=', a // b)  # 取商的整数部分
 print("逻辑运算符测试-----------------------------------")
-a=10
-if a<100 and a>0:
+a = 10
+if a < 100 and a > 0:
     print('100>a>0')
 else:
     print('a>=100或者a<=0')
 print("条件语句测试-----------------------------------")
-
-
-
-
